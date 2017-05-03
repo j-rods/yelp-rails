c7403b32e846f8ea4fefe73c182f15b996d14849
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-03 11:52:22 +0100

    add more validations

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 describe Restaurant, type: :model do
+
   it 'is not valid with a name of less than 3 characters' do
     restaurant = Restaurant.new(name: 'KF')
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: 'Pizza Hut')
+    restaurant = Restaurant.new(name: 'Pizza Hut')
+    expect(restaurant).to have(1).error_on(:name)
+  end
+
 end
