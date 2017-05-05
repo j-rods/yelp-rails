9cd6bbb221abfb2aa8feb1b538265179f639f148
kittysquee <kittysquee@gmail.com>
2017-05-05 16:58:07 +0100

    Need to fix JS and Endorse button

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -2,6 +2,7 @@ class EndorsementsController < ApplicationController
   def create
     @review = Review.find(params[:review_id])
     @review.endorsements.create
+    render json: {new_endorsement_count: @review.endorsements.count}
     redirect_to restaurants_path
   end
 end
